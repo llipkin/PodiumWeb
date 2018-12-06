be05f7e5a9928e7203ff2a7a8be2c4333f8dc3d1
trey <trlcoope@ucsc.edu>
2018-12-06 12:38:07 -0800

    scmmit

---
+++ src/main.js
@@ -0,0 +1,60 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
+
+var firebase = require('firebase')
+var config = {
+  apiKey: 'AIzaSyCFvJArErxvDuY3_qcO8ZCon58qdtkRAx8',
+  authDomain: 'iosbookapp.firebaseapp.com',
+  databaseURL: 'https://iosbookapp.firebaseio.com',
+  projectId: 'iosbookapp',
+  storageBucket: 'iosbookapp.appspot.com',
+  messagingSenderId: '1099160631159'
+}
+firebase.initializeApp(config)
+export const db = firebase.firestore()
+/*
+firebase.auth().createUserWithEmailAndPassword('trieveon.lee@gmail.com', '123456').catch(function (error) {
+  // Handle Errors here.
+  // var errorCode = error.code
+  var errorMessage = error.message
+  console.log(errorMessage)
+  // ...
+}) */
+console.log('ran this portion')
+console.log('placeholder = ' + location.value)
+
+firebase.auth().signInWithEmailAndPassword('trieveon.lee@gmail.com', '123456').catch(function (error) {
+  // Handle Errors here.
+  // var errorCode = error.code;
+  var errorMessage = error.message
+  console.log(errorMessage)
+
+  // ...
+})
+console.log('user key' + firebase.auth().currentUser.uid)
+// var location = document.querySelector('locName')
+
+firebase.firestore().collection('location').add({
+  name: 'Tokyo',
+  country: 'Japan'
+})
+
+const saveButton = document.querySelector('#saveButton')
+saveButton.addEventListener('click', function () {
+  console.log('trying to save')
+  // var location = document.querySelector('locName')
+  // var locationPicture = document.querySelector('locPic')
+})
